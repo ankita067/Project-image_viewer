17c4c892382c799e6036f1f1c1c095a846ccf798
ankita067 <45871129+ankita067@users.noreply.github.com>
2020-04-19 19:32:21 +0530

    Home page header code

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,30 +1,85 @@
 import React, { Component } from 'react';
 import './Home.css';
-
 import SearchIcon from '@material-ui/icons/Search';
-//import TextField from '@material-ui/core/TextField';
-//import InputLabel from '@material-ui/core/InputLabel';
-//import Input from '@material-ui/core/Input';
-//import SearchIcon from '@material-ui/icons/Search';
-//import AppBar from '@material-ui/core/AppBar';
-//  import FormControl from '@material-ui/core/FormControl';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
- 
-// import { fade, makeStyles } from '@material-ui/core/styles';
- import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+// import Modal from 'react-modal';
+// import Menu from '@material-ui/core/Select';
+// import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import { withStyles } from '@material-ui/core/styles';
+
+const customStyles = {
+    content: {
+        top: '60%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },
+})((props) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+));
 
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
 
+export function CustomizedMenus() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+}
 
 
 class Home extends Component {
     constructor() {
         super();
         this.state = {
-            User: {
-                data: {
+            isUserMenuOpen: false,
+            acess_token: sessionStorage.getItem("access-token"),
+            isLoggedIn: false,
+            userdata: [
+                {
                     id: "",
                     username: "",
                     profile_picture: "",
@@ -41,12 +96,64 @@ class Home extends Component {
                         code: ""
                     }
                 }
-            },
-            acess_token: sessionStorage.getItem("access-token")
+            ],
+            allImages: [
+                {
+                    id: "",
+                    user: [
+                        {
+                            id: "",
+                            full_name: "",
+                            profile_picture: "",
+                            username: ""
+                        }
+                    ],
+                    images: [
+                        {
+
+                        }
+                    ],
+                    created_time: "",
+                    caption: [],
+                    user_has_liked: false,
+                    likes: {
+                        count: ""
+                    }
+                    ,
+                    tags: [
+
+                    ],
+                    filter: "",
+                    comments: {
+                        count: ""
+                    },
+                    type: "",
+                    link: "",
+                    location: {},
+                    attribution: [],
+                    users_in_photo: []
+
+                }
+            ]
         };
     }
+    // const [anchorEl, setAnchorEl] = React.useState(null);
+    IconButtonClickHandler = (e) => {
+        // this.props.togglePopover();
+        // this.setAnchorEl(e.currentTarget);
+        this.setState({ isUserMenuOpen: true })
+    }
+
+    handleClose = (e) => {
+        // this.props.togglePopover();
+        // this.setAnchorEl(null);
+        this.setState({ isUserMenuOpen: false })
+    }
+
 
     UNSAFE_componentWillMount() {
+
+
 
         let that = this;
         let Userdata = null;
@@ -55,7 +162,8 @@ class Home extends Component {
             if (this.readyState === 4) {
 
                 that.setState({
-                    User: JSON.parse(this.responseText)
+                    userdata: JSON.parse(this.responseText).data,
+                    isLoggedIn: true
                 });
 
             }
@@ -64,27 +172,86 @@ class Home extends Component {
         xhrUser.open("GET", this.props.baseUrl + "users/self/?access_token=" + this.state.acess_token);
         xhrUser.setRequestHeader("Cache-Control", "no-cache");
         xhrUser.send(Userdata);
+
+
+        //Get Profile Picture 
+
+        let allimages = null;
+        let xhrAllImages = new XMLHttpRequest();
+        xhrAllImages.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                //alert(JSON.parse(this.responseText).data);
+                that.setState({
+                    allImages: JSON.parse(this.responseText).data
+                });
+                // console.log(that.state.allImages   );
+            }
+        });
+        xhrAllImages.open("GET", this.props.baseUrl + "users/self/media/recent?access_token=" + this.state.acess_token);
+        xhrAllImages.setRequestHeader("Cache-Control", "no-cache");
+        xhrAllImages.send(allimages);
+
     }
 
 
 
     render() {
+        let isLoggedIn = this.state.isLoggedIn;
+        const renderProfilePicture = () => {
+            if (isLoggedIn === "undefined") {
+                return "";
+            } else {
+                return <img src={this.state.userdata.profile_picture} className="ProfileIcon" alt={this.state.userdata.full_name} />
+
+            }
+        }
         return (
             <div>
-                
-                <div className="logo-Header">
-               
-                    <div className="logo">Image Viewer</div>
 
-                    
+                <div className="logo-Header">
+                    <div className="logo">Image Viewer</div>
                     <div className="search">
-                        <span className="searchIcon"><SearchIcon /></span>                   
-                        <input type="text" className="input"  placeholder="Search..." />
+                        <span className="searchIcon"><SearchIcon /></span>
+                        <input type="text" className="input" placeholder="Search..." />
                     </div>
-                    <div className="userImage"></div>
-                   
+
+                    <div className="userImage">
+                        <IconButton onClick={this.IconButtonClickHandler}>
+                            {renderProfilePicture()}
+                        </IconButton>
+                        <StyledMenu
+                            id="customized-menu"
+                            // anchorEl={anchorEl}
+                            keepMounted
+                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            open={this.state.isUserMenuOpen}
+                            onClose={this.handleClose}
+                        >
+                            <StyledMenuItem>
+                                {/* <ListItemIcon>
+                                    <SendIcon fontSize="small" />
+                                </ListItemIcon> */}
+                                <ListItemText primary="My Account" />
+                            </StyledMenuItem>
+                            <StyledMenuItem>
+                                {/* <ListItemIcon>
+                                    <DraftsIcon fontSize="small" />
+                                </ListItemIcon> */}
+                                <ListItemText primary="LogOut" />
+                            </StyledMenuItem>
+                            
+                        </StyledMenu>
+
+                    </div>
+                    <div>
+
+
+                    </div>
+
                 </div>
-                
+
+
+
 
 
             </div>
